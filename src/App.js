799e56ed1a63b1d7f1668d0c97ca4cc87d9f5f12
Neel <20it071@charusat.edu.in>
2023-05-26 11:13:48 +0530

    login and registration page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
+import React from 'react';
 import './App.css';
-import { HomePage } from './Components/HomePage';
-import { Route, Routes, Link, BrowserRouter } from 'react-router-dom';
-import { Apple } from './Components/Apple';
-import { NotFound } from './Components/NotFound';
-import 'react-toastify/dist/ReactToastify.css';
-
-const App = () => {
+import SignInOutContainer from './containers';
+function App() {
   return (
-  <BrowserRouter>
-      <div>
-        <Link to="/" style={{marginleft: 5,}}> Home </Link>
-        <Link to="/apple" style={{marginleft: 15,}}> Apple </Link>
-        <Link to="/applet" style={{marginleft: 30,}}> Applet </Link>
-      </div>
-      <Routes>
-        <Route path='/' element={<HomePage />}></Route>
-        <Route path='/apple' element={<Apple />}></Route>
-        <Route path='*' element={<NotFound />}></Route>
-      </Routes>
-        
-  </BrowserRouter>
+    <div className="App">
+     <SignInOutContainer/> 
+    </div>
   );
-}
+} 
 
 export default App;
